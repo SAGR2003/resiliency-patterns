cb946dae36db81b2a7854ed3d0c8ed4432d3992f
SAGR2003 <sagr20032006@gmail.com>
2024-04-04 00:29:39 -0500

    Add initial Circuit Breaker

--- pizzapi/src/main/java/com/example/pizzapi/controller/PizzaOrderController.java
+++ pizzapi/src/main/java/com/example/pizzapi/controller/PizzaOrderController.java
@@ -3,6 +3,8 @@ package com.example.pizzapi.controller;
 import com.example.pizzapi.model.PizzaOrder;
 import com.example.pizzapi.service.IPizzaOrder;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,8 +16,13 @@ public class PizzaOrderController {
     private final IPizzaOrder pizzaService;
 
     @GetMapping("")
-    public List<PizzaOrder> getAllOrders() {
-        return pizzaService.getAllOrders();
+    public ResponseEntity<?> getAllOrders() {
+        List<PizzaOrder> orders = pizzaService.getAllOrders();
+        if (orders.size() == 1 && orders.get(0).getCustomerName().equals("CircuitOpen")) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Open circuit");
+        } else {
+            return ResponseEntity.ok(orders);
+        }
     }
 
     @PostMapping("")
--- pizzapi/src/main/java/com/example/pizzapi/service/PizzaOrderService.java
+++ pizzapi/src/main/java/com/example/pizzapi/service/PizzaOrderService.java
@@ -2,24 +2,36 @@ package com.example.pizzapi.service;
 
 import com.example.pizzapi.model.PizzaOrder;
 import com.example.pizzapi.repository.PizzaOrderRepository;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Service
 @AllArgsConstructor
 public class PizzaOrderService implements IPizzaOrder{
-    private PizzaOrderRepository pizzaOrderRepository;
+    private final PizzaOrderRepository pizzaOrderRepository;
+    private final AtomicInteger requestCounter = new AtomicInteger(0);
 
     @Override
+    @CircuitBreaker(name = "pizzaDb", fallbackMethod = "fallbackGetAllOrders")
     public List<PizzaOrder> getAllOrders() {
+        int requestNumber = requestCounter.incrementAndGet();
+        if(requestNumber % 3 == 0){
+            throw new RuntimeException("Request multiple of 3");
+        }
         return pizzaOrderRepository.findAll();
     }
 
     @Override
     public PizzaOrder createOrder(PizzaOrder pizzaOrder) {
         return pizzaOrderRepository.save(pizzaOrder);
+    }
+
+    public List<PizzaOrder> fallbackGetAllOrders(Throwable t){
+        return Collections.singletonList(new PizzaOrder(1L,"CircuitOpen", "", ""));
     }
 }
